255cd52d6f920c90a586ab6bc9fef5360ac29e21
jfelipeschwaab <joaofsp.games@gmail.com>
2025-06-10 17:32:37 -0300

    Initial Commit

---
+++ WishList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WishList
+//
+//  Created by João Felipe Schwaab on 10/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WishList/WishListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishListApp.swift
+//  WishList
+//
+//  Created by João Felipe Schwaab on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct WishListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
